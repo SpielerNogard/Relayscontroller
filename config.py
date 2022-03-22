10eeb2ff7d847765535c46b59f79f6a60eded99b
Christopher Müller <63948181+SpielerNogard@users.noreply.github.com>
2022-03-22 14:50:54 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 
 #your MAD Server data
-server = "www.spielernogard.de"
-username = "Christopher" 
-password = "J3DJ1fuM"
-databasename = "pogomap"
+server = "your server"
+username = "your username" 
+password = "your password"
+databasename = "your database name"
 
 #List of your Devices
-mydevices= ["Dessau1","Dessau2","Dessau3","Dessau4","Dessau5","Dessau6","Dessau8","ChristopherKoethen5"]
+mydevices= ["Device1", "Device2"]
 
 #AFK time before restart in mins
 afktime = 60
